29cc5fa078e230ac4f6d4046414c252466e80def
ialmeida-caylent <isadora.almeida@caylent.com>
2026-05-19 14:18:36 -0300

    feat(glue): add bronze/silver dbs - glue catalog module

---
+++ terraform/ingestion/glue_catalog.tf
@@ -0,0 +1,13 @@
+# Glue Databases for layers (bronze / silver)
+resource "aws_glue_catalog_database" "databases" {
+  for_each = var.glue_databases
+
+  name        = "${each.value.name}-${var.environment}"
+  description = each.value.description
+
+  tags = merge(var.tags, {
+    Name        = "${each.value.name}-${var.environment}"
+    Environment = var.environment
+    Layer       = each.key
+  })
+}
