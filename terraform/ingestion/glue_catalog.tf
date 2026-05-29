097b8d4409a3c97d5691b56fcd4ef088e917163f
ialmeida-caylent <isadora.almeida@caylent.com>
2026-05-29 13:16:44 -0300

    fix(glue): glue raw DB prefix in raw

--- terraform/ingestion/glue_catalog.tf
+++ terraform/ingestion/glue_catalog.tf
@@ -7,7 +7,7 @@ resource "aws_glue_catalog_database" "databases" {
   name        = "${each.value.name}_${var.environment}"
   description = each.value.description
   # S3 paths for DB locations
-  location_uri = can(regex("bronze", each.key)) ? "s3://escr20-bronze-${var.environment}/" : can(regex("silver", each.key)) ? "s3://escr20-silver-${var.environment}/" : "s3://escr20-landing-zone-raw-${var.environment}/"
+  location_uri = can(regex("bronze", each.key)) ? "s3://escr20-bronze-${var.environment}/" : can(regex("silver", each.key)) ? "s3://escr20-silver-${var.environment}/" : "s3://escr20-landing-zone-raw-${var.environment}/raw-db/"
 
 
   tags = merge(var.tags, {
