61a0b0b78d86e105b41052fc9f5d047899311402
ialmeida-caylent <isadora.almeida@caylent.com>
2026-05-29 11:28:16 -0300

    fix(glue): adding raw DB for crawler

--- terraform/ingestion/glue_catalog.tf
+++ terraform/ingestion/glue_catalog.tf
@@ -4,9 +4,10 @@
 resource "aws_glue_catalog_database" "databases" {
   for_each = var.glue_databases
 
-  name         = "${each.value.name}_${var.environment}"
-  description  = each.value.description
-  location_uri = can(regex("bronze", each.key)) ? "s3://escr20-bronze-dev/" : "s3://escr20-silver-dev/"
+  name        = "${each.value.name}_${var.environment}"
+  description = each.value.description
+  # S3 bucket DB locations
+  location_uri = can(regex("bronze", each.key)) ? "s3://escr20-bronze-${var.environment}/" : can(regex("silver", each.key)) ? "s3://escr20-silver-${var.environment}/" : "s3://escr20-landing-zone-raw-${var.environment}/"
 
 
   tags = merge(var.tags, {
