517585a72bc61892d2946fc69fbe7ce27aeb2075
wmkahnlein <60745071+Wernerkahnlein@users.noreply.github.com>
2026-06-04 11:40:02 -0300

    fix: ami data ref (#112)
    * fix: rightsizing airbyte instance
    
    * fix: rightsizing airbyte instance
    
    * fix: fixing AMI data reference

--- terraform/ingestion/airbyte.tf
+++ terraform/ingestion/airbyte.tf
@@ -4,7 +4,7 @@
 data "aws_ssm_parameter" "al2023_ami" {
   count = var.create ? 1 : 0
 
-  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-arm64"
 }
 
 # ---------------------------------------------------------------------------
